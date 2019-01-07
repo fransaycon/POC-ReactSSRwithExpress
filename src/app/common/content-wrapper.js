a8950a5bbd6174d036d13adf7be572290a1402c0
Franrey Saycon <reyfran1111@gmail.com>
2019-01-07 11:21:37 +0800

    CSS -> StyleComponents

---
+++ src/app/common/content-wrapper.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from "styled-components";
+import { colors } from "../constants";
+
+export let ContentWrapper = styled.div`
+    width: 960px;
+    background-color: ${colors.white};
+    margin: auto;
+    height: 100vh;
+`
---
+++ src/app/home/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import MainWrapper from "../../common/main-wrapper";
+
+export default class App extends Component {
+    render(){
+        return (
+            <MainWrapper>
+                <center><h1>Hello World!</h1></center>
+            </MainWrapper>
+        );
+    }
+}

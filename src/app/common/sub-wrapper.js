4b8e6a6d85314cf536bc698cc462431deb061075
Franrey Saycon <reyfran1111@gmail.com>
2019-01-09 09:22:54 +0800

    Initial Layout Addition

---
+++ src/app/common/sub-wrapper.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from "styled-components";
+import { colors } from "../constants";
+
+
+export let BlueWrapper = styled.div`
+    display: flex;
+    height: 90vh;
+    background-color: ${colors.mainBlue};
+    padding-left: 10vw;
+    padding-right: 10vw;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
+
+export let WhiteWrapper = styled.div`
+    display: flex;
+    height: 90vh;
+    background-color: ${colors.white};
+    padding-left: 10vw;
+    padding-right: 10vw;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+`
--- src/app/constants.js
+++ src/app/constants.js
@@ -1,4 +1,5 @@
 export let colors = {
     grey: "#D3D3D3",
     white: "#FFFFFF",
+    mainBlue: "#4274c4",
 }
